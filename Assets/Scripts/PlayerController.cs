d8983a3f3c493248e3b367d15d7b0907fd677b30
Elliotpwnz <Elliotsminion@gmail.com>
2016-02-03 23:21:06 -0500

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	//Declare our key variables
+	public KeyCode left;
+	public KeyCode right;
+
+	//Our component for updating animations
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		//Set the key variables
+		left = KeyCode.A;
+		right = KeyCode.D;
+
+		//Set the animator component
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetBool("isRunning", (Input.GetKey("a") || Input.GetKey("d")));
+
+	}
+}
