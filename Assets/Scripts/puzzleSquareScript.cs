9e876913a2e2ff8ed9d898d11943d7431021435b
Elliot Simpson <Elliotsminion@gmail.com>
2016-02-11 23:16:36 -0500

    puzzle square prefab
    created a puzzle square prefab that generates a block if it contains a
    hidden block.

---
+++ Assets/Scripts/puzzleSquareScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class puzzleSquareScript : MonoBehaviour {
+
+	//Defines whether the puzzle square contains a hidden block.
+	public bool hiddenBlock;
+
+	//Our renderer component
+	Renderer rend; 
+
+	// Use this for initialization
+	void Start () {
+		//This square contains a hidden block
+		hiddenBlock = true;
+
+		//Our renderer component
+		rend = GetComponent<Renderer> ();
+	}
+
+	//If the game object is clicked
+	void OnMouseOver(){
+		if (Input.GetMouseButtonDown(0) && hiddenBlock) {
+			GameObject theBlock = (GameObject)Instantiate (Resources.Load ("platform"));
+			theBlock.transform.localScale = transform.localScale;
+			theBlock.transform.position = transform.position;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
